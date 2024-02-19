734ecc00696f793bfc275db1a05fdc79bf623578
Arafat-Bhuiyan <arafatbhuiyan41@gmail.com>
2024-02-19 23:44:54 +0600

    Triangle and rectangle calculation added

--- js/main.js
+++ js/main.js
@@ -6,8 +6,19 @@ function getInputFieldValueById(inputId){
     inputField.value = '';
     return inputFieldValue;
 }
+// Area of Triangle
 document.getElementById("triangle").addEventListener('click',function(){
     const triangleFirstValue = getInputFieldValueById("triangle-first-value");
     const triangleSecValue = getInputFieldValueById("triangle-sec-value");
-
+    const areaOfTriangle = .5 * triangleFirstValue * triangleSecValue;
+    console.log(areaOfTriangle);
 })
+
+// Area of Rectangle
+document.getElementById("rectangle").addEventListener('click',function(){
+    const rectangleFirstValue = getInputFieldValueById("rectangle-first-value");
+    const rectangleSecValue = getInputFieldValueById("rectangle-sec-value");
+    const areaOfRectangle = rectangleFirstValue * rectangleSecValue;
+    console.log(areaOfRectangle);
+})
+
